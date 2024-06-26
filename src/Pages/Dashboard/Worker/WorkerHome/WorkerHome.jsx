7fb6dc96aa2d879d11deddbe944021d379b79516
MushfikMahi <salehinmahi2004@gmail.com>
2024-06-26 15:01:26 +0600

    workar state added

--- src/Pages/Dashboard/Worker/WorkerHome/WorkerHome.jsx
+++ src/Pages/Dashboard/Worker/WorkerHome/WorkerHome.jsx
@@ -1,7 +1,56 @@
+import { FaCoins } from "react-icons/fa";
+import { FaFileImport } from "react-icons/fa";
+import { IoWalletOutline } from "react-icons/io5";
+import useAuth from "../../../../Hooks/useAuth";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import { useEffect, useState } from "react";
 const WorkerHome = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [state, setStates] = useState();
+  useEffect(() => {
+    axiosSecure(`/worker-state/${user?.email}`).then((result) => {
+      setStates(result.data);
+    });
+  }, []);
+
   return (
     <div>
-      <h2 className="text-5xl">hello from home</h2>
+      <section className="p-6 my-6 dark:bg-gray-100 dark:text-gray-800">
+        <div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 xl:grid-cols-4">
+          <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 dark:bg-gray-50 dark:text-gray-800">
+            <div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 dark:bg-violet-600">
+              <FaCoins className="text-3xl text-white" />
+            </div>
+            <div className="flex flex-col justify-center align-middle">
+              <p className="text-3xl font-semibold leading-none">
+                {state?.coin?.coin}
+              </p>
+              <p className="capitalize">Coin</p>
+            </div>
+          </div>
+          <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 dark:bg-gray-50 dark:text-gray-800">
+            <div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 dark:bg-violet-600">
+              <FaFileImport className="text-3xl text-white" />
+            </div>
+            <div className="flex flex-col justify-center align-middle">
+              <p className="text-3xl font-semibold leading-none">
+                {state?.totalSubmission}
+              </p>
+              <p className="capitalize">Total Submission</p>
+            </div>
+          </div>
+          <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 dark:bg-gray-50 dark:text-gray-800">
+            <div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 dark:bg-violet-600">
+              <IoWalletOutline className="text-3xl text-white" />
+            </div>
+            <div className="flex flex-col justify-center align-middle">
+              <p className="text-3xl font-semibold leading-none">172%</p>
+              <p className="capitalize">Growth</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
